43549b83a7389cc5d3cb6ba85d461b6ad3005cf1
Akash Raghav <akash.raghav@snapdeal.com>
2021-01-20 06:27:47 +0530

    add blank test case

--- src/test/StringCalculatorTest.kt
+++ src/test/StringCalculatorTest.kt
@@ -1,15 +1,21 @@
 package test
 
 import TDDContainer
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 
 class StringCalculatorTest {
 
-    lateinit var tddContainer: TDDContainer
+    private lateinit var tddContainer: TDDContainer
 
     @Before
     fun setup() {
         tddContainer = TDDContainer()
     }
 
+    @Test
+    fun test_add_emptyInput_zero() {
+        Assert.assertEquals(tddContainer.add(""), 0)
+    }
 }
